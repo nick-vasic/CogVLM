730f46b9dac5a89368e4823767200434f30f8c3c
Nick Vasic <>
2023-12-06 12:50:15 +1100

    Some temporary tracking prints

--- utils/chat.py
+++ utils/chat.py
@@ -42,6 +42,8 @@ def chat(image_path, model, text_processor, img_processor,
     if not history:
         history = []
 
+    print('Entered chat (1)')
+
     if no_prompt:
         query = ''
     prompt = text_processor.history_to_prompt(query, history)
@@ -54,6 +56,8 @@ def chat(image_path, model, text_processor, img_processor,
                 torch_image[k] = torch_image[k].to(next(model.parameters()).dtype)
             if type(torch_image[k]) is torch.Tensor:
                 torch_image[k] = torch_image[k].to(next(model.parameters()).device)
+    
+    print('------- (2)')
 
     inputs_dic = text_processor(prompt)
     for k in inputs_dic:
@@ -63,6 +67,7 @@ def chat(image_path, model, text_processor, img_processor,
             inputs_dic[k] = inputs_dic[k].to(next(model.parameters()).device)
     input_ids = inputs_dic['input_ids'].to(model.parameters().__next__().device)[0]
     
+    print('------- (3)')
     if max_length-len(input_ids) <= 1:
         response = "The prompt exceeds the context length limit, please try again."
         return response, history, (torch_image, pil_img)
@@ -70,17 +75,21 @@ def chat(image_path, model, text_processor, img_processor,
     seq = torch.cat(
         [input_ids, torch.tensor([-1]*(max_length-len(input_ids)), device=input_ids.device)], dim=0
     )
+    print('------- (4)')
     strategy = BaseStrategy(temperature=temperature, top_p=top_p, top_k=top_k, end_tokens=[text_processor.tokenizer.eos_token_id],
                             invalid_slices=invalid_slices, repetition_penalty=repetition_penalty)
+    print('------- (5)')
     # use beam search to get a better result
     # strategy = BeamSearchStrategy(temperature=temperature, top_p=top_p, top_k=top_k, end_tokens=[text_processor.tokenizer.eos_token_id],
     #                               num_beams=5, consider_end=True, repetition_penalty=repetition_penalty)
     get_func = text_processor.get_func(input_ids, **inputs_dic) if hasattr(text_processor, 'get_func') else get_masks_and_position_ids_default
 
+    print('------- (6)')
     img_inputs = {'vision_'+k: v for k, v in torch_image.items()}
     inputs_dic.pop('input_ids')
     inputs = {**img_inputs, **inputs_dic}
 
+    print('------- (7)')
     output = filling_sequence(
         model, seq,
         batch_size=1,
@@ -88,7 +97,8 @@ def chat(image_path, model, text_processor, img_processor,
         strategy=strategy,
         **inputs
     )[0] # drop memory
-    
+
+    print('------- (8)')
     # ---------------
     # port from inference_glm.py, more general than chat mode
     # clip -1s and fill back generated things into seq
@@ -97,13 +107,18 @@ def chat(image_path, model, text_processor, img_processor,
     else:
         output_list = output
 
+    print('------- (9)')
     response = text_processor.tokenizer.decode(output_list[0])
+    print('------- (10)')
     # print('original:', response)
     if hasattr(text_processor, 'process_response'):
         response = text_processor.process_response(response)
     response = response.split(text_processor.sep)[-1].strip()
+    print('------- (11)')
     if get_model_parallel_rank() == 0:
         from utils.parser import parse_response
         parse_response(pil_img, response)
+        print(response)
     history = history + [(query, response)]
+    print('------- (12)')
     return response, history, (torch_image, pil_img)
