44cc011031b7bc7c4af79cef2cd7c3126d115a8b
Nick Vasic <>
2023-12-11 16:01:31 +1100

    Fix flask config to listen on all IPs

--- message_publisher.py
+++ message_publisher.py
@@ -76,4 +76,5 @@ def chat():
         return jsonify({'error': 'No reply received within timeout period'}), 504
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
